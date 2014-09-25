2be9dfc67d55bf13b1ba3819a72a163d665d8e80
James Edward Gray II <james@graysoftinc.com>
2014-09-25 09:31:05 -0500

    Rethinking Square as a proper object.

--- lib/chess/square.rb
+++ lib/chess/square.rb
@@ -1,11 +1,20 @@
 module Chess
   class Square
-    def to_coordinate position 
-      x,y = position.scan(/\-?\d+|[a-z]+/)
-      [(x.ord - 'a'.ord),y.to_i-1]
+    def initialize(chess_notation)
+      @chess_notation = chess_notation
     end
-    def to_chess_notation coordinate 
-      "#{(coordinate[0]+'a'.ord).chr}#{coordinate[1]+1}"
+
+    # ... def valid?
+
+    def to_coordinates
+      fail "Bad notation" unless @chess_notation =~ /\A([a-h])(-?\d+)\z/
+
+      x, y = $1, $2
+      [x.ord - 'a'.ord, y.to_i - 1]
+    end
+
+    def to_s
+      @chess_notation
     end
   end
 end
